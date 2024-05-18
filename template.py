5cab3e90192a74c215e159c308f3bcf961851a13
PrateekDwivedi2991 <dwivedi.prateekhotmail.com>
2024-05-19 02:37:46 +0530

    structure created

---
+++ template.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+import logging
+
+list_of_files=[
+
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipline/__init__.py",
+    "src/pipline/training_pipline.py",
+    "src/pipline/prediction_pipeline.py",
+    "src/utils/utils.py",
+    "src/utils/__init__.py",
+    "src/logger/logging.py",
+    "src/exceptions/exceptions.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.config",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb"
+]
+
+for filepath in list_of_files:
+    # print(filepath)
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file :{filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass
+
